c03d3ea6a8077e5fbea19e8cdd2ba543bef149bb
Dinoop <dinoopm@gmail.com>
2023-11-25 23:49:00 +0100

    added page folder and tailwind css

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1 class="text-3xl font-bold underline">Restaurant finder</h1>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
