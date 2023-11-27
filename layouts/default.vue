27a7f38fdfd8be682ce8f434d60e873d78a3ed48
Dinoop <dinoopm@gmail.com>
2023-11-28 00:45:53 +0100

    html fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="bg-black p-5">
-        <h1 class="text-white text-3xl font-bold">Restaurant finder</h1>
-    </header>
-    <main>
-        <slot />
-    </main>
+  <header class="bg-black p-5">
+    <h1 class="text-white text-3xl font-bold">Restaurant finder</h1>
+  </header>
+  <div>
+    <slot />
+  </div>
 </template>
