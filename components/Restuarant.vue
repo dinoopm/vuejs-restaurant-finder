964b60b1b39a4c85a61e15c5ba6e4d70cca5ce06
Dinoop <dinoopm@gmail.com>
2023-11-29 00:18:56 +0100

    on dark mode hover color has been fixed

--- components/Restuarant.vue
+++ components/Restuarant.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="restuarant-item p-6 hover:bg-sky-300 cursor-pointer dark:bg-blue-950 dark:text-white"
+    class="restuarant-item p-6 hover:bg-sky-300 cursor-pointer dark:bg-blue-950 dark:text-white dark:hover:bg-white dark:hover:text-black"
     :class="{ selected: id === business.id }"
   >
     <p class="font-bold text-xl">{{ business.name }}</p>
