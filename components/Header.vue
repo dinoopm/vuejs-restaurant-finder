519fc0c59533154a8886d8398f8697309d2a1232
Dinoop <dinoopm@gmail.com>
2023-11-29 02:56:15 +0100

    fix for rendering mismatch

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,17 @@
 <template>
   <header class="bg-black p-5 flex">
     <h1 class="text-white text-3xl font-bold w-3/4">Restaurant finder</h1>
-    <button
-      class="text-white w-1/4 text-right"
-      @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')"
-    >
-      <span v-if="$colorMode.value == 'light'">Dark mode</span>
-      <span v-else="$colorMode.value == 'dark'">Light mode</span>
-    </button>
+    <ClientOnly>
+      <button
+        class="text-white w-1/4 text-right"
+        @click="
+          setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')
+        "
+      >
+        <span v-if="$colorMode.value == 'light'">Dark mode</span>
+        <span v-else="$colorMode.value == 'dark'">Light mode</span>
+      </button>
+    </ClientOnly>
   </header>
 </template>
 
